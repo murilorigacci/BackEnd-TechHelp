35ca151b2dd7614b4dad48ebfb588c2136bfca99
jennifer <jennifer.k.silva7@aluno.senai.br>
2025-12-07 01:06:43 -0300

    feat: formata arquivos

--- src/routes/techHelpRoute.js
+++ src/routes/techHelpRoute.js
@@ -1,15 +1,15 @@
-import { Router } from 'express';
-import * as techHelpController from './../controllers/techHelpController.js'
+import { Router } from "express";
+import * as techHelpController from "./../controllers/techHelpController.js";
 
 const router = Router();
 
-router.get('/status', techHelpController.listarStatus);
-router.get('/prioridade', techHelpController.listarPrioridade);
-router.get('/responsavel', techHelpController.listarResponsavel);
-router.get('/', techHelpController.listarTodos);
-router.get('/:id', techHelpController.listarUm);
-router.post('/', techHelpController.criar);
-router.put('/:id', techHelpController.atualizar);
-router.delete('/:id', techHelpController.apagar);
+router.get("/status", techHelpController.listarStatus);
+router.get("/prioridade", techHelpController.listarPrioridade);
+router.get("/responsavel", techHelpController.listarResponsavel);
+router.get("/", techHelpController.listarTodos);
+router.get("/:id", techHelpController.listarUm);
+router.post("/", techHelpController.criar);
+router.put("/:id", techHelpController.atualizar);
+router.delete("/:id", techHelpController.apagar);
 
 export default router;
